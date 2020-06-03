4a376f85ef45db6c729c52e5418152d6c13de83f
Pankaj <pankajakhade@gmail.com>
2020-06-03 12:10:04 +0530

    .

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,10 +1,12 @@
 #!/usr/bin/env groovy
 node{
     try {
-        stage('Create Network'){
-            cmd = "gcloud compute networks create my-vpc --project=" + params.project +
-                   " --subnet-mode=custom --bgp-routing-mode=regional"
-            sh cmd
+        stageLabel = params.action + " Network"
+        stage(stageLabel){
+            def networksCmdMap = ["create": "gcloud compute networks create my-vpc --project=" + params.project +
+                   " --subnet-mode=custom --bgp-routing-mode=regional",
+                   "delete": "gcloud compute networks delete my-vpc --project=" + params.project + " --quiet"]
+            sh networksCmdMap[params.action]
         }
     } catch(Exception e){
         println(e)
