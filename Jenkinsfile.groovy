50f14e1dd085598f9b161dfc65f32d61d2876b4b
Pankaj <pankajakhade@gmail.com>
2020-06-03 12:01:46 +0530

    .

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,11 +1,12 @@
 #!/usr/bin/env groovy
-
-try {
-    stage('Create Network'){
-        cmd = "gcloud compute networks create my-vpc --project=" + params.project +
-               "--subnet-mode=custom --bgp-routing-mode=regional"
-        sh cmd
+node{
+    try {
+        stage('Create Network'){
+            cmd = "gcloud compute networks create my-vpc --project=" + params.project +
+                   "--subnet-mode=custom --bgp-routing-mode=regional"
+            sh cmd
+        }
+    } catch(Exception e){
+        println(e)
     }
-} catch(Exception e){
-    println(e)
 }
