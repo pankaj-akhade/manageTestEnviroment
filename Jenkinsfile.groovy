706e0bf83f3e659e7008c2e1ee34da870ff01b9c
Pankaj <pankajakhade@gmail.com>
2020-06-03 12:57:50 +0530

    .

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,20 +3,20 @@
 def flowForCreateEnv = ["Network", "Subnetwork"]
 def flowForDeleteEnv = ["Subnetwork", "Network"]
 
-def manageNetwork(){
+def manageNetwork(String action){
     def networksCmdMap = ["create": "gcloud compute networks create " +  params.clusterName + "-vpc --project=" +
                    params.project + " --subnet-mode=custom --bgp-routing-mode=regional",
                    "delete": "gcloud compute networks delete " +  params.clusterName + "-vpc --project=" +
                    params.project + " --quiet"]
-    sh networksCmdMap[params.action]
+    sh networksCmdMap[action]
 }
 
-def manageSubnetwork(){
+def manageSubnetwork(String action){
     def networksCmdMap = ["create": "gcloud compute networks subnets create " +  params.clusterName +
            "-subnet --project=" + params.project + " --range=10.34.0.0/20 --network=" +
            params.clusterName + "-vpc --region=us-east1",
            "delete": "gcloud compute networks delete my-vpc --project=" + params.project + " --quiet"]
-    sh networksCmdMap[params.action]
+    sh networksCmdMap[action]
 }
 
 node{
