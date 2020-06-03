7fad1ccb97cfa6019e13f52ab62cd9b72934b37c
Pankaj <pankajakhade@gmail.com>
2020-06-03 15:45:49 +0530

    .

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,21 +1,21 @@
 #!/usr/bin/env groovy
 
-def flowForCreateEnv = ["network", "subnetwork"]
-def flowForDeleteEnv = ["subnetwork", "network"]
+def flowForCreateEnv = ["vpc", "subnet"]
+def flowForDeleteEnv = ["subnet", "vpc"]
 
-def managenetwork(String action){
-    def networksCmdMap = ["create": "gcloud compute networks create " +  params.clusterName + "-vpc --project=" +
-                   params.project + " --subnet-mode=custom --bgp-routing-mode=regional",
-                   "delete": "gcloud compute networks delete " +  params.clusterName + "-vpc --project=" +
+def manageVpc(String action, String resource){
+    def networksCmdMap = ["create": "gcloud compute networks create " +  params.clusterName + "-" + resource +
+                   " --project=" + params.project + " --subnet-mode=custom --bgp-routing-mode=regional",
+                   "delete": "gcloud compute networks delete " +  params.clusterName + "-" + resource + " --project=" +
                    params.project + " --quiet"]
     sh networksCmdMap[action]
 }
 
-def managesubnetwork(String action){
-    def networksCmdMap = ["create": "gcloud compute networks subnets create " +  params.clusterName +
-           "-subnet --project=" + params.project + " --range=10.34.0.0/20 --network=" +
+def manageSubnet(String action, String resource){
+    def networksCmdMap = ["create": "gcloud compute networks subnets create " +  params.clusterName + "-" + resource +
+           " --project=" + params.project + " --range=10.34.0.0/20 --network=" +
            params.clusterName + "-vpc --region=us-east1",
-           "delete": "gcloud compute networks delete " +  params.clusterName + "-subnet --project=" +
+           "delete": "gcloud compute networks delete " +  params.clusterName + "-" + resource + " --project=" +
            params.project + " --quiet"]
     sh networksCmdMap[action]
 }
@@ -24,16 +24,16 @@ node{
     if (params.action == "create"){
         for(String resource in flowForCreateEnv){
             stage(params.action + ' ' + resource){
-                def methodName = 'manage' + resource
-                "$methodName"(params.action)
+                def methodName = 'manage' + resource.capitalize()
+                "$methodName"(params.action, resource)
             }
         }
     } else if (params.action == "delete"){
         for(String resource in flowForDeleteEnv){
             stage(params.action + ' ' + resource){
-                def methodName = 'manage' + resource
+                def methodName = 'manage' + resource.capitalize()
                 try{
-                    "$methodName"(params.action)
+                    "$methodName"(params.action, resource)
                 }catch(error){
                     println(error)
                     println(params.clusterName + "-" + resource + " does not exists. Skipping..")
