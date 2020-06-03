07b8ad98f37adfc6c95e37ac422c453567eadf2d
Pankaj <pankajakhade@gmail.com>
2020-06-03 12:03:17 +0530

    .

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,7 +3,7 @@ node{
     try {
         stage('Create Network'){
             cmd = "gcloud compute networks create my-vpc --project=" + params.project +
-                   "--subnet-mode=custom --bgp-routing-mode=regional"
+                   " --subnet-mode=custom --bgp-routing-mode=regional"
             sh cmd
         }
     } catch(Exception e){
