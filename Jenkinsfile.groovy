0308661fd7bbec03a758ad9d6383ce0ab407d8d8
Pankaj <pankajakhade@gmail.com>
2020-06-08 20:34:41 +0530

    .

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -147,7 +147,7 @@ def manageSecret(String action, String resource){
 
 def manageExtdns(String action, String resource){
     if(action == "create"){
-        sh "sed 's/<project-id>/" + params.project + "/g' external-dns.yaml"
+        sh "sed -i 's/<project-id>/" + params.project + "/g' external-dns.yaml"
         sh "kubectl create -f external-dns.yaml"
     }
 }
@@ -156,7 +156,7 @@ def manageNfs(String action, String resource){
     if(action == "create"){
         def fsIp = sh (script: "gcloud filestore instances describe " + params.envName + "-filestore --zone=" + params.region +
           "-b --format=\"json(networks)\" | jq .networks[0].ipAddresses[0] | tr -d '\"'", returnStdout: true).trim()
-        sh "sed 's/<project-id>/$fsIp/g' nfs-client/deployment.yaml"
+        sh "sed -i 's/<project-id>/$fsIp/g' nfs-client/deployment.yaml"
         sh "kubectl create namespace storage"
         sh "kubectl create -f nfs-client/serviceaccount.yaml"
         sh "kubectl create -f nfs-client/clusterrole.yaml"
